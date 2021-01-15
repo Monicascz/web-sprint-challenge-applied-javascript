5f108c16931d8f4b726a4a2f81695a2e622dc52f
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-01-15 08:25:23 -0800

    header rendering with my own inserted date, time, and title

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,6 @@
-const Header = (title, date, temp) => {
+
+
+const Header = ({title, date, temp}) => {
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -11,7 +13,28 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
+const header = document.createElement('div')
+  header.classList.add('header')
+
+const spanDate = document.createElement('span')
+  spanDate.classList.add('date')
+  spanDate.textContent = date;
+
+const H1 = document.createElement('h1') 
+  H1.textContent = title;
+
+const spanTemp = document.createElement('span')
+  spanTemp.classList.add('temp')
+  spanTemp.textContent = temp;
+  const headerCont = document.querySelector('.header-container')
+headerCont.appendChild(header)
+header.appendChild(spanDate)
+header.appendChild(H1)
+header.appendChild(spanTemp)
+
+return header
+
+}; console.log(Header({title: "test", date: "today", temp: "75"}))
 
 const headerAppender = (selector) => {
   // TASK 2
